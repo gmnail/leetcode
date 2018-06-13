19d3254fc848f95d5f60bd20676af71f30a4f376
郭旻 <m18112146077_1@163.com>
2018-06-13 21:04:10 +0800

    3.Longest_substring

---
+++ 3.Longest_substring/longest_char.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int lengthOfLongestSubstring(char* s) {
+    int len = strlen(s);
+	int count = 0;
+	int start = 0;
+	bool isOk = true;
+	
+	int longlength = 0;
+	
+	for(int i=0; i<len; i++)
+	{
+		isOk = true;
+		for(int j=0; j<count; j++)
+		{
+			if(s[j+start] == s[i])
+			{
+				isOk = false;
+				start = j+start+1;	//不能用i
+				break;
+			}
+		}
+		
+		if(isOk) {
+			count++;
+			if(i==len-1&&longLenth<count){
+                longLenth=count;
+            }
+		}
+		else {
+			if(longLenth<count){
+                longLenth=count;
+            }
+			count = i-start+1;
+		}
+	}
+	return longLenth;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
